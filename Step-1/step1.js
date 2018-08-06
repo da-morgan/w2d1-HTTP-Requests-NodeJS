63046e538b2082e04b78f48cdcae1449c1dd3358
David Morgan <david.alexandermorgan@gmail.com>
2018-08-06 18:01:34 +0000

    Step 1 working

---
+++ Step-1/step1.js
@@ -0,0 +1,23 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.request(requestOptions, function(response){
+    console.log('making request');
+    response.on('data' ,function(chunk){
+      console.log('chunk recieved');
+      console.log(chunk);
+    });
+
+  }).end();
+
+  console.log('request made');
+
+}
+
+getAndPrintHTMLChunks();
