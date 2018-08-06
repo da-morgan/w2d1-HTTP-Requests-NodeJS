35ba8e0d1745899e2a3a4baa503b0e94ba8b5f57
David Morgan <david.alexandermorgan@gmail.com>
2018-08-06 18:04:44 +0000

    Chunks now converted to readable strings

--- Step-1/step1.js
+++ Step-1/step1.js
@@ -11,7 +11,7 @@ function getAndPrintHTMLChunks () {
     console.log('making request');
     response.on('data' ,function(chunk){
       console.log('chunk recieved');
-      console.log(chunk);
+      console.log(chunk.toString());
     });
 
   }).end();
