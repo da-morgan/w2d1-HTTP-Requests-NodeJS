14a0503d1323effe8bb8506e9efd7a023e4321f5
David Morgan <david.alexandermorgan@gmail.com>
2018-08-06 18:42:05 +0000

    Step 3 now working

---
+++ Step-3/step3.js
@@ -0,0 +1,29 @@
+var https = require('https');
+
+function getAndPrintHTML (options) {
+  var data ='';
+
+    https.get(options, function(response){
+    response.setEncoding('utf8');
+
+    response.on('data' ,function(chunk){
+      data += chunk + '\n';
+    });
+
+    response.on('end', function(){
+      console.log(data);
+    });
+
+  });
+
+  console.log('request made successfully');
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
+
